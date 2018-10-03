714006cbb6ca174eb03e838c6029c3e1978f5fac
Subham Misra <chromium45@gmail.com>
2018-10-03 18:24:01 +0000

    second commit

---
+++ node-examples/index.js
@@ -0,0 +1,20 @@
+var rect={
+    perimeter:(x,y) => (2*(x+y)),
+    area:(x,y) => (x*y)
+};
+function solveRect(l,b){
+    console.log("solving for rectangle with l=" + l + "and b=" + b);
+    if (l<=0||b<=0)
+        {
+            console.log("Rectangle dimension should be greter than zero");
+        }
+    else{
+    console.log("The perimeter of the rectangle is:" + rect.perimeter(l,b));
+    console.log("The area of the rectangle is:" + rect.area(l,b));
+    
+    }
+}
+solveRect(23,45)
+solveRect(0,45)
+solveRect(2,5)
+solveRect(23,-45)
